2d28d7cd9190a665678a623d3b0276b28a54423d
jgph91 <jgph91@gmail.com>
2019-11-21 16:17:24 +0100

    distances

--- SRC/functions.py
+++ SRC/functions.py
@@ -55,3 +55,21 @@ def google_query(query):
         time.sleep(2)
     return places
 
+# functions that returns video games companies which are at an specified max dist
+
+def dist(e,max_distance):
+    final_db, final_coll = connectCollection('New_York','Final_New_York')
+    video_games_locations = list(final_coll.find({'$and':[
+        {'category':'Video games companies'},
+       {"coordinates": {
+         '$near': {
+           '$geometry': {
+              'type': "Point" ,
+              'coordinates': [e['coordinates'][0],e['coordinates'][1]]
+           },
+
+           '$maxDistance': max_distance
+         }
+       }
+    }]}))
+    return video_games_locations
